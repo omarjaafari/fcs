58faf0278555704129066b03d626485ac6cab790
omarjaafari <omarjaafari2006@gmail.com>
2025-10-04 23:24:52 +0300

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,25 @@
+username = "admin"
+password = "1234"
+system = True
+for i in range(3):
+    username_input = input("username: ")
+    password_input = input("password: ")
+    if username == username_input and password == password_input:
+        system = True
+        break
+    else:
+        system = False
+        if i == 2:
+            print ("account locked")
+        else:
+            print ("wrong username or password")
+            print("------------------------------------------")
+if system:
+    N = int(input("Enter a number: "))
+    for num in range(2, N + 1):
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            print(num)
+
