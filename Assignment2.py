d40a6522245296612c7c56a5c1eb89a5ca68826e
omarjaafari <omarjaafari2006@gmail.com>
2025-10-05 09:44:59 +0300

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -5,14 +5,15 @@ for i in range(3):
     username_input = input("username: ")
     password_input = input("password: ")
     if username == username_input and password == password_input:
+        system = True
         break
     else:
         system = False
-        if i == 2:
-            print ("account locked")
-        else:
+        if i < 2:
             print ("wrong username or password")
             print("------------------------------------------")
+        else:
+            print ("account locked")
 if system:
     N = int(input("Enter a number: "))
     for num in range(2, N + 1):
@@ -21,5 +22,6 @@ if system:
                 break
         else:
             print(num)
+
 
 
