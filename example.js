59ffd9af4ce118dea6154d491173b4cf987b1d86
omarjaafari <omarjaafari2006@gmail.com>
2025-11-02 11:27:07 +0200

    Add files via upload

---
+++ example.js
@@ -0,0 +1,19 @@
+// 1
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// 2
+let student = {
+    name: "Alice",
+    score: 95
+};
+console.log(student);
+
+// 3
+class Phone {
+    call() {
+        console.log("Calling...");
+    }
+}
+
